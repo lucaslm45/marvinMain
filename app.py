73664d61226dbd6ae6553ba2b1c83febe230569a
zapherk <raphaelfelix@alunos.utfpr.edu.br>
2022-06-22 21:01:12 -0300

    Criado o método para receber as coordenadas do frontend

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
-from flask import Flask
+import json
+from urllib import response
+from flask import Flask, Response, request
+from flask_cors import cross_origin
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@cross_origin()
+def status():
+    response = {
+        "status": True,
+        "info": "Hello! Welcome to Marvin API!"
+    }
+    return Response(json.dumps(response), status=200, mimetype='application/json')
+
+@app.route('/api/coordinates', methods=['POST'])
+@cross_origin()
+def post_coordinates():
+    body = request.json
+    print(body['first_coordinate'])
+    print(body['second_coordinate'])
+    print(body['third_coordinate'])
+
+    # Adicionar código que chama a função main com base nas coordenadas.
+
+    return Response(status=200, mimetype='application/json')
 
